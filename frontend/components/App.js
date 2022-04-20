5714114984f88d440e64599dcdc23edb9d49c01e
brad <bradley.michaels@outlook.com>
2022-04-19 20:46:02 -0400

    initial TODOS

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
+const Todos = [
+  {
+    name: 'Organize Garage',
+    id: 1528817077286, // could look different, you could use a timestamp to generate it
+    completed: false
+  },
+  {
+    name: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+]
+
 export default class App extends React.Component {
   render() {
     return (
       <div>
+        
         Todo App
       </div>
     )
